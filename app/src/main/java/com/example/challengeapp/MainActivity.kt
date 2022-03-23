d8ec951ce75a216c3532da8a9800295d059a68ee
김영주 <kimyj@encar.com>
2022-03-23 23:54:09 +0900

    edit main activity

---
+++ app/src/main/java/com/example/challengeapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.challengeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.challengeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val challengeList = arrayListOf(
+            Challenge(0, R.drawable.water, "Drink Water", "하루에 2리터 이상 물마시기!", true)
+        )
+
+        binding.rvChallenge.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvChallenge.setHasFixedSize(true)
+
+        binding.rvChallenge.adapter = ChallengeAdapter(challengeList)
+    }
+}
