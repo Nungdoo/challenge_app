a75365e09ebe207cff10d02e4a308c0e48d69c08
김영주 <kimyj@encar.com>
2022-03-24 21:52:13 +0900

    add challenge click event (move DetailActivity)

--- app/src/main/java/com/example/challengeapp/ChallengeAdapter.kt
+++ app/src/main/java/com/example/challengeapp/ChallengeAdapter.kt
@@ -1,19 +1,29 @@
 package com.example.challengeapp
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class ChallengeAdapter(val challengeList: ArrayList<Challenge>) : RecyclerView.Adapter<ChallengeAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChallengeAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+
         return CustomViewHolder(view).apply {
             itemView.setOnClickListener {
-                // 상세페이지로 이동
+                val curPos = adapterPosition
+                val id = challengeList.get(curPos).id
+                Toast.makeText(parent.context, "${id}", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(itemView.context, DetailActivity::class.java)
+                intent.putExtra("id", id)
+                ContextCompat.startActivity(itemView.context, intent, null)
             }
         }
     }
